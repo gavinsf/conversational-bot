bbdd1f635fdf4466f5d79cfc52dee51e58766b89
gavinsf <gsfoster031@gmail.com>
2026-07-30 13:47:11 -0700

    Adds Customer and Message model

--- back/models.py
+++ back/models.py
@@ -1,7 +1,7 @@
 import enum
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import Enum as SQLEnum
-from sqlalchemy import ForeignKey, String, DateTime
+from sqlalchemy import ForeignKey, String, DateTime, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from back.database import Base
 from sqlalchemy.dialects.postgresql import UUID
@@ -19,20 +19,26 @@ class Channel(enum.Enum):
     voice          = "mobile"
     sms            = "sms"
 
+class SenderType(enum.Enum):
+    customer       = "customer"
+    bot            = "bot"
+    agent          = "agent"
+    system         = "system"
+
 class Conversation(Base):
-    __tablename__ = "conversation"
+    __tablename__ = "conversations"
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    customer_id : Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("customers.id"))
-    last_intent : Mapped[str | None] = mapped_column(String, nullable=True)
+    customer_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("customers.id"))
+    last_intent: Mapped[str | None] = mapped_column(String, nullable=True)
     status: Mapped[ConversationStatus] = mapped_column(
         SQLEnum(ConversationStatus), default=ConversationStatus.bot_handling
     )
     channel: Mapped[Channel] = mapped_column(
-        SQLEnum.Channel, default = Channel.web_chat
+        SQLEnum(Channel), default = Channel.web_chat
     )
     last_intent: Mapped[str | None] = mapped_column(String, nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
-    up_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc),
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc),
                                             onupdate=datetime.now(timezone.utc))
 
     customer: Mapped["Customer"] = relationship(back_populates="conversations")
@@ -41,4 +47,30 @@ class Conversation(Base):
     )
     escalation: Mapped["EscalationTicket | None"] = relationship(
         back_populates="conversation", uselist=False, cascade="all, delete-orphan"
+    )
+
+class Customer(Base):
+    __tablename__ = "customers"
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name: Mapped[String] = mapped_column(String, nullable=False)
+    email: Mapped[String] = mapped_column(String, unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
+
+    conversations: Mapped[list["Conversation"]] = relationship(
+        back_populates="customer", cascade="all, delete-orphan"
+    )
+
+class Message(Base):
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    conv_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("conversations.id"))
+    sender: Mapped[SenderType] = mapped_column(SQLEnum(SenderType), nullable=False)
+    text: Mapped[String] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
+
+    # For bot
+    intent: Mapped[String | None] = mapped_column(String)
+    confidence: Mapped[Float | None] = mapped_column(Float)
+
+    conversation: Mapped["Conversation"] = mapped_column(
+        back_populates="message", cascade="all, delete-orphan"
     )
