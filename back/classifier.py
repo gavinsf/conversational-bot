aca038e6244a4bd76ade144fc1ed63ed8c460dee
gavinsf <gsfoster031@gmail.com>
2026-07-31 18:02:20 -0700

    Fixes intent bug

--- back/classifier.py
+++ back/classifier.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, APIRouter
 from back.schemas import ChatResponse, ChatText
 from back.config import settings
+import uuid
 
 chat_router = APIRouter(prefix="/api/chat")
 
 @chat_router.post("/", response_model=ChatResponse)
-async def intent_classifier(text: ChatText, session_id):
+async def intent_classifier(text: ChatText, session_id: str | None = None):
+    if not session_id:
+        session_id = str(uuid.uuid4())
+    
     async with settings.session.client("lexv2-runtime", region_name=settings.AWS_REGION) as client:
         response = await client.recognize_text(
             botId = settings.BOT_ID,
@@ -21,6 +25,6 @@ async def intent_classifier(text: ChatText, session_id):
         return ChatResponse(
             messages=response.get("messages", []),
             session_state=response.get("sessionState", {}),
-            session_id=response.get("sessionId", ""),
-            confidence_score=top.get("nluConfidence", {})
+            session_id=session_id,   
+            confidence_score=top.get("nluConfidence", {}).get("score") if top else None
         )
--- back/config.py
+++ back/config.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
 import os
-import boto3
-import json
+import aioboto3
+
+load_dotenv()
 
 class Settings():
     DB_URL = os.getenv("DB")
     DEBUG = True
+    BOT_ID = os.getenv("LEX_BOT_ID")
+    BOT_ALIAS_ID = os.getenv("LEX_BOT_ALIAS_ID")
+    LOCALE_ID = os.getenv("LEX_LOCALE_ID")
+    AWS_REGION = os.getenv("AWS_REGION")
+    session = aioboto3.Session()
 
 
 settings = Settings()
